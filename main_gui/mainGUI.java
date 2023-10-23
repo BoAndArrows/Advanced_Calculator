c68350bed5f9520b86e9e088115ec2d3c60a3f85
bocheung <bocheung15@gmail.com>
2023-10-23 14:04:21 -0400

    Silly comment also testing commit

--- main_gui/mainGUI.java
+++ main_gui/mainGUI.java
@@ -1,7 +1,11 @@
 package main_gui;
 
 public class mainGUI {
-    
+    /*
+    *   To be impleted after first functions are added
+    *   I plan on there being somekind of selection menu
+    *   later down the line
+    */
     public static void main(String args[]){
         System.out.println("hello!");
     }
